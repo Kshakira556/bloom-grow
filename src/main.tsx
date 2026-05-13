8c2fc7daa4a2f9b6a13352d9a5aeefe581f9d013
kshakira556 <kshakira556@gmail.com>
2026-05-13 12:28:40 +0200

    Fix: bundled fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
+/* Nunito */
+import "@fontsource/nunito/400.css";
+import "@fontsource/nunito/500.css";
+import "@fontsource/nunito/600.css";
+import "@fontsource/nunito/700.css";
+import "@fontsource/nunito/800.css";
+
+/* Quicksand */
+import "@fontsource/quicksand/400.css";
+import "@fontsource/quicksand/500.css";
+import "@fontsource/quicksand/600.css";
+import "@fontsource/quicksand/700.css";
+
 // Get the root element
 const container = document.getElementById("root")!;
 
